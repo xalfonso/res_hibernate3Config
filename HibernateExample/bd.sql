f9b8ab83e26513b50ed014219e0486d777279093
Eduardo Alfonso Sanchez <eddie.alfonso@gmail.com>
2016-10-01 11:56:29 -0500

    ..
    ...

---
+++ HibernateExample/bd.sql
@@ -0,0 +1,42 @@
+/*
+SQLyog Community v12.2.4 (64 bit)
+MySQL - 5.7.11 : Database - testdb
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`testdb` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `testdb`;
+
+/*Table structure for table `employee` */
+
+DROP TABLE IF EXISTS `employee`;
+
+CREATE TABLE `employee` (
+  `ID` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `NAME` varchar(255) NOT NULL,
+  `ROLE` varchar(255) NOT NULL,
+  `insert_time` timestamp NOT NULL,
+  PRIMARY KEY (`ID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+/*Data for the table `employee` */
+
+insert  into `employee`(`ID`,`NAME`,`ROLE`,`insert_time`) values 
+(1,'Pankaj','CEO','2016-07-22 13:32:09'),
+(2,'Lisa','Manager','2016-07-22 13:41:37'),
+(3,'David','Developer','2016-07-22 13:42:01'),
+(4,'Eduardo','CEO','2016-07-22 14:31:08');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
